28001b5ebbd97a917cfc227bb8624ba0457a6658
ralintuscano <ralintuscano16@student.sfit.ac.in>
2020-10-22 19:54:46 +0530

    added the Modal Component

--- components/Series.js
+++ components/Series.js
@@ -7,6 +7,7 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import SeriesModal from "./SeriesModal";
 
 const useStyles = makeStyles({
   root: {
@@ -39,9 +40,7 @@ function Series() {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary" variant="contained">
-          Read Plot
-        </Button>
+        <SeriesModal />
       </CardActions>
     </Card>
   );
--- components/SeriesModal.js
+++ components/SeriesModal.js
@@ -1,7 +1,71 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
+import Button from "@material-ui/core/Button";
 
-function SeriesModal() {
-  return <div>Modaal</div>;
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[3],
+    padding: theme.spacing(2, 4, 3),
+    maxWidth: 400,
+    outline: "none",
+  },
+}));
+
+export default function TransitionsModal() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button
+        size="small"
+        color="primary"
+        variant="contained"
+        onClick={handleOpen}
+      >
+        Read Plot
+      </Button>
+      <Modal
+        aria-labelledby="series-title"
+        aria-describedby="series-plot"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <h2 id="series__title">Batman Returns</h2>
+            <p id="series__plot">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde
+              eligendi necessitatibus minima minus laborum saepe reprehenderit
+              maiores numquam, magnam nulla dicta qui voluptas vero quibusdam
+              harum facere excepturi enim veniam?
+            </p>
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  );
 }
-
-export default SeriesModal;
